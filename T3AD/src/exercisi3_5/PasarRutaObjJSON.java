4a1e938b9385ce87b0f183d09f6eea872fa7fa22
amo11 <rub.amoros11@gmail.com>
2015-10-16 16:15:19 +0200

    putamierda

--- T3AD/src/exercisi3_5/PasarRutaObjJSON.java
+++ T3AD/src/exercisi3_5/PasarRutaObjJSON.java
@@ -1,22 +1,48 @@
 package exercisi3_5;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Writer;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import exercici3_2.Ruta;
 
 public class PasarRutaObjJSON {
-	public static void main(String[] args) throws ClassNotFoundException, IOException{
+	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		ObjectInputStream f = new ObjectInputStream(new FileInputStream("Ruta.obj"));
 		Ruta r = (Ruta) f.readObject();
-		
-		JSONArray rutes = (JSONArray) ((JSONObject) ((JSONArray ) new JSONParser().parse(f)).get(0)).get("ocupacion");
-		
+
+		JSONArray rutaDatos = new JSONArray();
+		JSONArray puntsArray = new JSONArray();
+
+		JSONObject datos = new JSONObject();
+		JSONObject arrel = new JSONObject();
+		JSONObject punts = new JSONObject();
+
+		for (int i = 0; i < r.length(); i++) {
+			punts.put("num", i + 1);
+			punts.put("nom", r.getPuntNom(i));
+			punts.put("latitud", r.getPuntLatitud(i));
+			punts.put("altidu", r.getPuntLongitud(i));
+			puntsArray.add(punts);
+		}
+
+		while (true) {
+			datos.put("nom", r.getNom());
+			datos.put("denivell", r.getDesnivell());
+			datos.put("desnivell acumulat", r.getDesnivellAcumulat());
+			datos.put("punts", puntsArray);
+			rutaDatos.add(datos);
+
+			arrel.put("rutes", rutaDatos);
+
+			Writer w_json = new FileWriter("rutes.json");
+			w_json.write(arrel.toJSONString());
+			w_json.close();
+		}
 	}
 }
